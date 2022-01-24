68a361c8de6aa57c753d23ad2880348b9c61abde
viktor-soderborg-nti-johanneberg <74763822+viktor-soderborg-nti-johanneberg@users.noreply.github.com>
2022-01-24 19:34:41 +0100

    init

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// Generera bräde (alla kort är en färg för att visa att de är nervända, kan använda states)
+// Vända två kort (visa vad som gömmer sig bakom bakgrundsfärgen)
+// Kolla ifall de är likadana (ge poäng & håll de uppvända vid korrekt, vänd ner ifall fel)
+// Kolla ifall brädet är tomt/alla par är hittade
+
+
+// TEST KORT
+// let img = document.querySelector('div img');
+// let test = document.querySelector('div').addEventListener('click', changeOpacity);
+// function changeOpacity() {
+//     img.classList.toggle('opacity')
+// }
