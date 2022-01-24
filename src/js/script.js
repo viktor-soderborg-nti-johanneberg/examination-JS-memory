0120eea9288ddbf5d3d18fca771167ec4d8154fd
viktor-soderborg-nti-johanneberg <74763822+viktor-soderborg-nti-johanneberg@users.noreply.github.com>
2022-01-24 20:09:41 +0100

    Code planning

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,61 @@
 
 // TEST KORT
 // let img = document.querySelector('div img');
-// let test = document.querySelector('div').addEventListener('click', changeOpacity);
+// let div = document.querySelector('div')
+// div.addEventListener('click', changeOpacity);
 // function changeOpacity() {
 //     img.classList.toggle('opacity')
 // }
+
+let cards = [];
+let flippedCards = [];
+let counter = document.querySelector('main p');
+
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", displayCard);
+};
+
+function displayCard() {
+    //flippa kortet och inte tillåta användaren att trycka det igen tills den är flippad tillbaka
+}
+
+//shuffle array function
+
+let deck = document.querySelector('section');
+
+function startGame() {
+    //call shuffle function with cards array
+
+    // FIXA DETTA
+    // /*shuffleArray*/.forEach(el => {
+        // deck.insertAdjacentHTML('afterbegin', el);
+    // })
+}
+
+function flipCard() {
+    flippedCards.push(this);
+
+    if (flippedCards.length === 2) {
+        updateMoves();
+
+        if (flippedCards[0].type === flippedCards[1].type) {
+            match();
+        } else {
+            noMatch();
+        }
+    }
+}
+
+function match() {
+    //ta bort korten men inte HTML objektet för grid
+}
+
+function noMatch() {
+    //vänd om kortet igen
+}
+
+
+function updateMoves() {
+    moves++;
+    counter.innerText = `Antal drag: ${moves}`
+}
