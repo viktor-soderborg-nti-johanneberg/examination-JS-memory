827632e198618da2274bb4b7a36bf77d826eebe0
viktor-soderborg-nti-johanneberg <74763822+viktor-soderborg-nti-johanneberg@users.noreply.github.com>
2022-02-21 16:46:01 +0100

    V2 Final & Safety Measures

--- src/js/script.js
+++ src/js/script.js
@@ -1,273 +1,31 @@
-// 1.0
-// let main = document.querySelector('main');
-// let tileMap = document.querySelector('main section');
-// let types = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12];
-// let tiles = []
-// let flippedTiles = [];
-// let counter = document.querySelector('main p');
-// let moves = 0;
-// let clearedTiles = []
-// let backgrounds = "";
-
-// Blanda kortens position
-// function shuffle(array) {
-//     for (let i = array.length - 1; i > 0; i--) {
-//         let j = Math.floor(Math.random() * (i + 1));
-//         let temp = array[i];
-//         array[i] = array[j];
-//         array[j] = temp;
-//     }
-//     return array;
-// }
-
-// Generera bräde, alla kort är en färg för att visa att de är nervända
-// function startGame() {
-//     types = shuffle(types);
-    
-//     for (let i = 0; i < types.length; i++) {
-//         let tile = {
-//             img: `img/char-${types[i]}.png`
-//         }
-//         tiles.push(tile);
-//     };
-    
-//     for(let i = 0; i < tiles.length; i++) {
-//         let el = `<article><img src="${tiles[i].img}"></article>`
-//         tileMap.insertAdjacentHTML('beforeend', el);
-//     };
-
-//     backgrounds = document.querySelectorAll('main section > article');
-
-//     for (let i = 0; i < backgrounds.length; i++) {
-//         backgrounds[i].addEventListener('click', (e) => {
-//             displayTile(e.target);
-//         })
-//     };
-// }
-
-// Vända två kort (visa vad som gömmer sig bakom bakgrundsfärgen)
-// function displayTile(element) {
-    // Flippa kortet och inte tillåta användaren att trycka det igen tills den är flippad tillbaka
-//     element.parentNode.classList.toggle('opacity');
-//     flippedTiles.push(element);
-//     element.style.pointerEvents = 'none';
-//     element.parentNode.style.pointerEvents = 'none';
-//     setTimeout(checkTiles,1000);
-// }
-
-// Kolla ifall de är likadana
-// function checkTiles() {
-    
-//     if (flippedTiles.length === 2) {
-//         updateMoves();
-        
-//         if (flippedTiles[0].getAttribute('src').split(/[-.]/)[1] === flippedTiles[1].getAttribute('src').split(/[-.]/)[1]) {
-//             match(flippedTiles);
-//         } else {
-//             noMatch(flippedTiles);
-//         }
-//     }
-// }
-
-// Visa att korten är rätta
-// function match(elements) {
-//     // Ändra bakgrundsfärgen för korten så man vet att man har hittat dem
-//     elements.forEach(el => {
-//         el.parentNode.classList.add('found');
-//         el.parentNode.classList.remove('opacity');
-//         clearedTiles.push(el.parentNode);
-//     });
-//     flippedTiles = []
-//     gameOver();
-// }
-
-// Vänd ner ifall fel och tillåt användaren att trycka på dem igen
-// function noMatch(elements) {
-    // Vänd om korten igen
-//     elements.forEach(el => {
-//         el.parentNode.classList.toggle('opacity');
-//         el.style.pointerEvents = '';
-//         el.parentNode.style.pointerEvents = '';
-//     });
-//     flippedTiles = [];
-// }
-
-// function updateMoves() {
-//     moves++;
-//     counter.innerText = `Antal drag: ${moves}`
-// }
-
-// Kolla ifall brädet är tomt/alla par är hittade
-// function gameOver() {
-//     let victoryScreen = 
-//     `<h2>Grattis du klarade det med ${moves} drag!</h2>
-//     <button onclick="location.reload()">Play again?</button>`
-
-//     if (clearedTiles.length === types.length) {
-//         tileMap.remove();
-//         counter.remove();
-//         main.insertAdjacentHTML('afterbegin', victoryScreen);
-//     }
-// }
-
-// Förbereda och generera spelet när sidan laddas in
-// startGame();
-
-// 2.0
-// let main = document.querySelector('main');
-// let tileMap = document.querySelector('main section');
-// let types = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12];
-// let flippedTiles = [];
-// let counter = document.querySelector('main p');
-// let moves = 0;
-// let clearedTiles = []
-// let backgrounds = "";
-// let background = [];
-
-// Blanda kortens position
-// function shuffle(array) {
-//     for (let i = array.length - 1; i > 0; i--) {
-//         let j = Math.floor(Math.random() * (i + 1));
-//         let temp = array[i];
-//         array[i] = array[j];
-//         array[j] = temp;
-//     }
-//     return array;
-// }
-
-// Generera bräde, alla kort är en färg för att visa att de är nervända
-// function startGame() {
-//     types = shuffle(types);
-    
-//     for (let i = 0; i < types.length; i++) {
-//         let tile = {
-//             img: `img/char-${types[i]}.png`,
-//             flipped: false,
-//             found: false,
-//             type: types[i]
-//         }
-//         let el = `<article><img src="${tile.img}"></article>`
-//         tileMap.insertAdjacentHTML('beforeend', el);
-//     };
-//     backgrounds = document.querySelectorAll('main section > article');
-
-//     for (let i = 0; i < backgrounds.length; i++) {
-//         backgrounds[i].addEventListener('click', (e) => {
-//             displayTile(e.target, e.path[1]);
-//         })
-//     };
-// }
-
-// Vända två kort (visa vad som gömmer sig bakom bakgrundsfärgen)
-// function displayTile(element, bg) {
-    // Flippa kortet och inte tillåta användaren att trycka det igen tills den är flippad tillbaka
-    // tile.flipped = true;
-//     background.push(bg);
-//     bg.classList.toggle('opacity');
-//     flippedTiles.push(element);
-//     element.style.pointerEvents = 'none';
-//     bg.style.pointerEvents = 'none';
-    // setTimeout(checkTiles,1000);
-// }
-
-// Kolla ifall de är likadana
-// function checkTiles() {
-    
-//     if (flippedTiles.length === 2) {
-//         updateMoves();
-
-//         if (flippedTiles[0].getAttribute('src').split(/[-.]/)[1] === flippedTiles[1].getAttribute('src').split(/[-.]/)[1]) {
-            // if(tile1.type === tile2.type) {}
-            // match(flippedTiles, background);
-//         } else {
-//             noMatch(flippedTiles, background);
-//         }
-//     }
-// }
-
-// Visa att korten är rätta
-// function match(elements, backg) {
-    // Ändra bakgrundsfärgen för korten så man vet att man har hittat dem
-    // backg.forEach(background => {
-    //     background.classList.add('found');
-    //     background.classList.remove('opacity');
-    //     clearedTiles.push(background);
-    // });
-    // flippedTiles = [];
-    // background = [];
-    // tile1.found = true;
-    // tile1.flipped = false,
-    // tile2.found = true;
-    // tile2.flipped = false
-    // gameOver();
-// }
-
-// Vänd ner ifall fel och tillåt användaren att trycka på dem igen
-// function noMatch(elements, backg) {
-    // Vänd om korten igen
-    // backg.forEach(background => {
-    //     background.classList.toggle('opacity');
-    //     elements.forEach(el => {
-    //         el.style.pointerEvents = ''  
-    //     });
-    //     background.style.pointerEvents = '';
-    // });
-    // flippedTiles = [];
-    // background = [];
-    // tile1.flipped = false;
-    // tile2.flipped = false;
-// }
-
-// function updateMoves() {
-//     moves++;
-    // counter.innerText = `Antal drag: ${moves}`
-// }
-
-// Kolla ifall brädet är tomt/alla par är hittade
-// function gameOver() {
-//     let victoryScreen = 
-//     `<h2>Grattis du klarade det med ${moves} drag!</h2>
-//     <button onclick="location.reload()">Play again?</button>`
-
-//     if (clearedTiles.length === types.length) {
-//         tileMap.remove();
-//         counter.remove();
-//         main.insertAdjacentHTML('afterbegin', victoryScreen);
-//     }
-// }
-
-// Förbereda och generera spelet när sidan laddas in
-// startGame();
-
-// 2.1
 let Game = {
     tiles: [
-        {img: 'img/char-1.png', flipped: false, found: false, type: 1},
-        {img: 'img/char-1.png', flipped: false, found: false, type: 1},
-        {img: 'img/char-2.png', flipped: false, found: false, type: 2},
-        {img: 'img/char-2.png', flipped: false, found: false, type: 2},
-        {img: 'img/char-3.png', flipped: false, found: false, type: 3},
-        {img: 'img/char-3.png', flipped: false, found: false, type: 3},
-        {img: 'img/char-4.png', flipped: false, found: false, type: 4},
-        {img: 'img/char-4.png', flipped: false, found: false, type: 4},
-        {img: 'img/char-5.png', flipped: false, found: false, type: 5},
-        {img: 'img/char-5.png', flipped: false, found: false, type: 5},
-        {img: 'img/char-6.png', flipped: false, found: false, type: 6},
-        {img: 'img/char-6.png', flipped: false, found: false, type: 6},
-        {img: 'img/char-7.png', flipped: false, found: false, type: 7},
-        {img: 'img/char-7.png', flipped: false, found: false, type: 7},
-        {img: 'img/char-8.png', flipped: false, found: false, type: 8},
-        {img: 'img/char-8.png', flipped: false, found: false, type: 8},
-        {img: 'img/char-9.png', flipped: false, found: false, type: 9},
-        {img: 'img/char-9.png', flipped: false, found: false, type: 9},
-        {img: 'img/char-10.png', flipped: false, found: false, type: 10},
-        {img: 'img/char-10.png', flipped: false, found: false, type: 10},
-        {img: 'img/char-11.png', flipped: false, found: false, type: 11},
-        {img: 'img/char-11.png', flipped: false, found: false, type: 11},
-        {img: 'img/char-12.png', flipped: false, found: false, type: 12},
-        {img: 'img/char-12.png', flipped: false, found: false, type: 12}
+        {img: 'img/char-1.png', flipped: false, found: false, type: 1, id: 1},
+        {img: 'img/char-1.png', flipped: false, found: false, type: 1, id: 2},
+        {img: 'img/char-2.png', flipped: false, found: false, type: 2, id: 3},
+        {img: 'img/char-2.png', flipped: false, found: false, type: 2, id: 4},
+        {img: 'img/char-3.png', flipped: false, found: false, type: 3, id: 5},
+        {img: 'img/char-3.png', flipped: false, found: false, type: 3, id: 6},
+        {img: 'img/char-4.png', flipped: false, found: false, type: 4, id: 7},
+        {img: 'img/char-4.png', flipped: false, found: false, type: 4, id: 8},
+        {img: 'img/char-5.png', flipped: false, found: false, type: 5, id: 9},
+        {img: 'img/char-5.png', flipped: false, found: false, type: 5, id: 10},
+        {img: 'img/char-6.png', flipped: false, found: false, type: 6, id: 11},
+        {img: 'img/char-6.png', flipped: false, found: false, type: 6, id: 12},
+        {img: 'img/char-7.png', flipped: false, found: false, type: 7, id: 13},
+        {img: 'img/char-7.png', flipped: false, found: false, type: 7, id: 14},
+        {img: 'img/char-8.png', flipped: false, found: false, type: 8, id: 15},
+        {img: 'img/char-8.png', flipped: false, found: false, type: 8, id: 16},
+        {img: 'img/char-9.png', flipped: false, found: false, type: 9, id: 17},
+        {img: 'img/char-9.png', flipped: false, found: false, type: 9, id: 18},
+        {img: 'img/char-10.png', flipped: false, found: false, type: 10, id: 19},
+        {img: 'img/char-10.png', flipped: false, found: false, type: 10, id: 20},
+        {img: 'img/char-11.png', flipped: false, found: false, type: 11, id: 21},
+        {img: 'img/char-11.png', flipped: false, found: false, type: 11, id: 22},
+        {img: 'img/char-12.png', flipped: false, found: false, type: 12, id: 23},
+        {img: 'img/char-12.png', flipped: false, found: false, type: 12, id: 24},
     ],
-    moves: -1,
+    moves: 0,
     counter: document.querySelector('main p'),
     clicked: [],
 
@@ -290,6 +48,8 @@ let Game = {
             let tile = this.tiles[i];
             let el = document.createElement('article');
 
+            tile.classList = "";
+
             if (tile.flipped) {
                 el.classList.add('opacity');
             }
@@ -298,7 +58,7 @@ let Game = {
                 el.classList.add('found');
             }
             
-            el.innerHTML = `<img src="${tile.img}">`;
+            el.innerHTML = `<img src="${tile.img}" alt="tile">`;
 
             el.addEventListener('click', (e) => {
                 // handle click
@@ -307,34 +67,42 @@ let Game = {
             main.appendChild(el);
         };
 
-        // Update moves
+        let filter = this.tiles.filter(tile => tile.found)
+        if (filter.length === this.tiles.length) {
+            this.gameOver();
+        }
+    },
+    updateMoves() {
         this.moves++
         this.counter.innerHTML = `<p>Antal drag: ${this.moves}</p>`;
-
-        // if(all tiles.found = true) {
-            // gameOver();
-        // }
     },
-    handleClick(i){
+    handleClick(i) {
         this.tiles[i].flipped = true;
         this.clicked.push(this.tiles[i]);
+        
+        this.updateUI();
 
         if (this.clicked.length === 2) {
-            setTimeout(() => {
-                if (this.clicked[0].type === this.clicked[1].type) {
-                    console.log(this.clicked);
-                    this.clicked[0].found = true;
-                    this.clicked[1].found = true;
-                    this.clicked = [];
-                } else {
-                    console.log(this.clicked);
-                    this.clicked[0].flipped = false;
-                    this.clicked[1].flipped = false;
-                    this.clicked = [];
-                }
-            }, 1000);
+            if (this.clicked[0].id === this.clicked[1].id) {
+                this.clicked.pop();
+            } else {
+                this.updateMoves();
+                setTimeout(() => {
+                    if (this.clicked[0].type === this.clicked[1].type) {
+                        this.clicked[0].found = true;
+                        this.clicked[1].found = true;
+                        this.clicked[0].flipped = false;
+                        this.clicked[1].flipped = false;
+                        this.clicked = [];
+                    } else {
+                        this.clicked[0].flipped = false;
+                        this.clicked[1].flipped = false;
+                        this.clicked = [];
+                    }
+                    this.updateUI();
+                }, 1000);
+            }
         }
-        this.updateUI();
     },
     startGame() {
         this.shuffle(this.tiles)
@@ -343,9 +111,10 @@ let Game = {
     gameOver() {
         let victoryScreen = 
         `<h2>Grattis du klarade det med ${this.moves} drag!</h2>
+        <img src="img/victory.gif" alt="Victory Gif">
         <button onclick="location.reload()">Play again?</button>`
 
-        document.querySelector('main').insertAdjacentHTML('afterbegin', victoryScreen);
+        document.querySelector('main').innerHTML = victoryScreen;
     },
 }
 
